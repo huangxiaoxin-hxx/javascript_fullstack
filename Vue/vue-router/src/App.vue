1912c1b569e6dc9677e1f8d01d118af7caafe341
huangxiaoxin-hxx <1228620998@qq.com>
2019-10-30 18:56:12 +0800

    use vue-cli

---
+++ Vue/vue-router/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-link to="/">首页</router-link>
+    <router-link to="/mine">我的</router-link>
+    <router-link to="/detail">详情</router-link>
+    <p><router-link to="/mine/mine2">我的子页面</router-link></p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/vue-router/src/components/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="detail">
+    这是一个详情页
+    <p>{{$route.params.id}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
