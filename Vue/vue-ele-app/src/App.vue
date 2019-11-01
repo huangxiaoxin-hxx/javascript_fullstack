6cab86d196becc2ecf6eeea1e17727b05ae35714
huangxiaoxin-hxx <1228620998@qq.com>
2019-11-01 18:59:58 +0800

    Vue ele-app firstProject

---
+++ Vue/vue-ele-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header.vue'
+export default {
+  name: 'App',
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vue-ele-app/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import header from '@/components/header/header.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/header',
+      name: 'Header',
+      component: header
+    }
+  ]
+})
