e3f7bbfa45594d592537224298913784500f49c8
huangxiaoxin-hxx <1228620998@qq.com>
2019-11-02 16:37:07 +0800

    element 饿了么 头部实现

--- Vue/vue-ele-app/src/components/header/header.vue
+++ Vue/vue-ele-app/src/components/header/header.vue
@@ -2,30 +2,93 @@
   <div class="header">
     <div class="content-wrapper">
       <div class="avatar">
-        <img src="./brand@2x.png" alt />
+        <img :src="seller.avatar" width="64" height="64" />
       </div>
       <div class="content">
         <div class="title">
           <span class="brand"></span>
-          <span class="name">粥品香坊</span>
+          <span class="name">{{seller.name}}</span>
         </div>
-        <div class="description">蜂鸟送达/38分钟送达</div>
-        <div class="support">
-          <span class="icon special"></span>
-          <span class="text">在线支付满28减5</span>
+        <div class="description">{{seller.description}}/{{seller.deliveryTime}}分钟送达</div>
+        <div class="support" v-if="seller.supports">
+          <span class="icon" :class="classMap[seller.supports[1].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div class="support-count" v-if="seller.supports" @click="detailShow">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" alt="" width="100%" height="100%">
+    </div>
+    <transition name="fade">
+      <div class="detail" v-show="showDetail">
+        <div class="detail-wrapper">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <!-- 星星 -->
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul class="supports">
+              <li class="support-item" v-for="(item, index) in seller.supports" :key="index">
+                <span class="icon" :class="classMap[item.type]"></span>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="detail-close" @click="detailClose">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  creade () {
-    this.$http.get('http://ustbhuangyi.com/sell/api/seller')
-      .then((res) => {
-        console.log(res)
-      })
+  props: {
+    seller: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
+      showDetail: false
+    }
+  },
+  methods: {
+    detailShow () {
+      this.showDetail = true
+    },
+    detailClose () {
+      this.showDetail = false
+    }
   }
 }
 </script>
@@ -78,12 +141,11 @@ export default {
           margin-right 4px
           background-size 12px 12px
           background-repeat no-repeat
-
           &.decrease
             bg-image('decrease_1')
           &.discount
             bg-image('discount_1')
-          &.&.guarantee
+          &.guarantee
             bg-image('guarantee_1')
           &.invoice
             bg-image('invoice_1')
@@ -92,4 +154,139 @@ export default {
         .text
           line-height 12px
           font-size 10px
-</style>
+    .support-count
+      position absolute
+      right 12px
+      bottom 14px
+      height 24px
+      line-height 24px
+      padding 0 8px
+      font-size 10px
+      border-radius 14px
+      text-align center
+      background rgba(0, 0, 0, .2)
+      .count
+        vertical-align top
+      .icon-keyboard_arrow_right
+        margin-left 2px
+        line-height 24px
+  .bulletin-wrapper
+    position relative
+    height 24px
+    line-height 24px
+    padding 0 22px 0 12px
+    white-space nowrap
+    overflow hidden
+    text-overflow ellipsis
+    background-color rgba(7, 17, 27, .2)
+    .bulletin
+      display inline-block
+      // margin-top 8px
+      width 22px
+      height 12px
+      bg-image('bulletin')
+      background-size 22px 12px
+      background-repeat no-repeat
+      vertical-align middle
+    .bulletin-text
+      font-size 10px
+      margin 0 4px
+      vertical-align middle
+    .icon-keyboard_arrow_right
+      position absolute
+      right 12px
+      top 8px
+      font-size 10px
+  .background
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index -1
+    filter blur(10px)
+  .detail
+    position fixed
+    top 0
+    left 0
+    right 0
+    bottom 0
+    overflow auto
+    background-color rgba(7, 17, 27, .8)
+    backdrop-filter blur(10px)
+    &.fade-enter,
+    &.fade-leave-to
+      opacity 0
+      background rgba(7, 17, 27, 0)
+    &.fade-enter-active,
+    &.fade-leave-active
+      transition all .5s;
+    &-wrapper
+      width 100%
+      min-height 100%
+      overflow auto
+      .detail-main
+        margin-top 64px
+        padding-bottom 64px
+        .name
+          line-height 16px
+          font-size 16px
+          text-align center
+          font-weight 700
+        .title
+          display flex
+          width 80%
+          margin 28px auto 24px auto
+          .line
+            border-bottom 1px solid rgba(255, 255, 255, .2)
+            flex 1
+            position relative
+            top -6px
+          .text
+            flex 1
+            text-align center
+        .supports
+          width 80%
+          margin 0 auto
+          .support-item
+            padding 0 12px
+            margin-bottom 12px
+            font-size 0
+            &:last-child
+              margin-bottom 0
+            .icon
+              display inline-block
+              width 16px
+              height 16px
+              vertical-align top
+              margin-right 6px
+              background-size 16px 16px
+              background-repeat no-repeat
+              &.decrease
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              line-height 16px
+              font-size 12px
+        .bulletin
+          width 80%
+          margin 0 auto
+          .content
+            padding 0 12px
+            line-height 24px
+            font-size 12px
+    .detail-close
+      position relative
+      width 32px
+      height 32px
+      margin -64px auto 0 auto
+      font-size 32px
+      clear both
+  </style>
