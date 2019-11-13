608078e726b9bdbd70e64485e9c96ec1a92b2e21
huangxiaoxin-hxx <1228620998@qq.com>
2019-11-13 21:30:45 +0800

    element UI 学习使用

--- Vue/vue-ele-app/src/components/ratings/ratings.vue
+++ Vue/vue-ele-app/src/components/ratings/ratings.vue
@@ -36,6 +36,59 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='stylus' scoped>
+.overview
+  display flex
+  padding 18px 0
+  .overview-left
+    -webkit-box-flex 0
+    -ms-flex 0 0 137px
+    flex 0 0 137px
+    padding 6px 0
+    width 137px
+    border-right 1px solid rgba(7,17,27,.1)
+    text-align center
+    .score
+      margin-bottom 6px
+      line-height 28px
+      font-size 24px
+      color #f90
+    .title
+      margin-bottom 8px
+      line-height 12px
+      font-size 12px
+      color #07111b
+    .rank
+      line-height 10px
+      font-size 10px
+      color #93999f
+  .overview-right
+    -webkit-box-flex 1
+    flex 1
+    padding 6px 0 6px 24px
+    .score-warpper
+      margin-bottom 8px
+      font-size 0
+      .title
+        display inline-block
+        line-height 18px
+        vertical-align top
+        font-size 12px
+        color #07111b
+      .star
+        display inline-block
+        margin 0 12px
+        vertical-align top
+      .score
+        display inline-block
+        line-height 18px
+        vertical-align top
+        font-size 12px
+        color #f90
+    .delivery-wrapper
+      font-size 0
+      .title
+        line-height 18px
+        font-size 12px
+        color #07111b
 </style>
