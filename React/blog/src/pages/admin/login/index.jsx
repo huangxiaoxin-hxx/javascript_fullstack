2420a958bad6f351a624d276b876903a7930af3a
huangxiaoxin-hxx <1228620998@qq.com>
2020-03-01 23:13:24 +0800

    blog 第二天

--- React/blog/src/pages/admin/login/index.jsx
+++ React/blog/src/pages/admin/login/index.jsx
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
-import { From, Input, Button, Card, Message } from 'antd'
+import { Form, Input, Button, Card, Message } from 'antd'
 import './index.less'
+import { UserOutlined,LockOutlined } from '@ant-design/icons'
+import { MenuItem } from 'rc-menu';
+import Particles from 'react-particles-js';
 
 class Login extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      loading: false
+      loading: false,
+      username: '',
+      password: ''
     }
   }
+
+
+  handleSubmit = (e) => {
+    // e.preventDefault()
+    // console.log(this.state.username, this.state.password)
+    console.log('登录成功')
+    sessionStorage.setItem('blogUser', '黄小心')
+    sessionStorage.setItem('menuItemKey', '0')
+    this.props.history.push('/admin/page')
+  }
+
   render() {
     return (
       <div className="login">
-        <Card className="login-from" style={{width: 300, borderRadius: 4}}>
-          hello world
+        <Particles/>
+        <Card className="login-form" style={{width: 300, borderRadius: 4}}>
+          <Form onFinish={this.handleSubmit}>
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: '请输入用户名!' }]}
+            >
+              <Input prefix={<UserOutlined style={{color: 'rgba(0, 0, 0, 0.25)'}} />} placeholder="请输入用户名" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: '请输入密码!' }]}
+            >
+              <Input prefix={<LockOutlined style={{color: 'rgba(0, 0, 0, 0.25)'}} />} placeholder="请输入密码" />
+            </Form.Item>
+
+            <Button type="primary" htmlType="submit" className="login-form-button" block >login</Button>
+          </Form>
         </Card>
       </div>
     );
