393048ce2914b8ceeb3fe25b927410da31e28831
huangxiaoxin-hxx <1228620998@qq.com>
2020-03-13 12:38:43 +0800

    react-redux end

--- React/react-redux/src/TodoList.jsx
+++ React/react-redux/src/TodoList.jsx
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import store from './store'
-import connect from 'react-redux'
+import { connect } from 'react-redux'
 
 class TodoList extends Component {
-  constructor (props) {
-    super(props)
-    this.state = store.getState()
-  }
-
   render () {
     return (
       <div>
         <div>
-          <input value={this.state.inputValue} type="text"/>
-          <button>提交</button>
+          <input value={this.props.inputValue} onChange={this.props.changeInputValue} type="text"/>
+          <button >提交</button>
         </div>
         <ul>
           <li>Dell</li>
@@ -23,4 +17,26 @@ class TodoList extends Component {
   }
 }
 
-export default TodoList;
+const mapStateToProps = (state) => {
+  return {
+    inputValue: state.inputValue
+  }
+}
+
+// connect 就是做连接
+// TodoList 和 store 做连接
+// mapStateToProps 的参数就是 store 该方法会帮助我们把store映射到当前的Props
+
+const mapDispatchToProps = (dispatch) => { // 把 store.dispatch 挂载到 props 上
+  return {
+    changeInputValue (e) {
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
