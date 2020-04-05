6362147fefc9c43b762b517ab83f1facc15cb7d8
huangxiaoxin-hxx <1228620998@qq.com>
2020-04-05 22:21:43 +0800

    next 3

--- React/next/nextjs-GitHub/pages/index.jsx
+++ React/next/nextjs-GitHub/pages/index.jsx
@@ -1,3 +1,44 @@
-import '../test/test.css'
+import Link from 'next/Link'
+import Router from 'next/router'
+import { Button } from 'antd'
 
-export default () => <span>index</span>
+const events = [
+  'routeChangeStart',
+  'routeChangeComplete',
+  'routeChangeError',
+  'beforeHistoryChange',
+  'hashChangeStart',
+  'hashChangeComplete',
+]
+
+function makeEvent(type) {
+  return (...args) => {
+    console.log(type, ...args)
+  }
+}
+
+events.forEach(event => {
+  Router.events.on(event, makeEvent(event))
+})
+
+export default () => {
+
+  function gotoTestB() {
+    Router.push({
+      pathname: '/test/b',
+      query: {
+        id: 2
+      }
+    }, '/test/b/2')
+  }
+
+  return (
+    <>
+      <Link href='/a?id=1' as='/a/1'>
+        <Button>Index</Button>
+      </Link>
+      <Button onClick={gotoTestB}>test b</Button>
+    </>
+
+  )
+}
--- React/next/nextjs-GitHub/readme.md
+++ React/next/nextjs-GitHub/readme.md
@@ -1,1 +1,11 @@
 # next默认不支持css,但是提供了插件来引入css
+ 需要配置next.config.js 文件
+
+ # Router 钩子
+
+ routeChangeStart
+ routeChangeComplete
+ routeChangeError
+ beforeHistoryChange
+ hashChangeStart
+ hashChangeComplete
