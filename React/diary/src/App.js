eccf14cb4408767251f02ef2f9146bd19e5e21a7
huangxiaoxin-hxx <1228620998@qq.com>
2020-04-29 13:34:14 +0800

    diary 初始化

---
+++ React/diary/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd-mobile'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">test</Button>
+    </div>
+  );
+}
+
+export default App;
